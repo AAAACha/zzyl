77a7dcc626a9f751d5c6048f4d76b5747fca46d9
AvA <14929406+ava_workspace@user.noreply.gitee.com>
2024-10-20 15:51:33 +0800

    Hutool工具常见方法

--- zzyl-web/src/test/java/com/zzyl/test/HttpUtilTest.java
+++ zzyl-web/src/test/java/com/zzyl/test/HttpUtilTest.java
@@ -7,6 +7,7 @@ import cn.hutool.json.JSONUtil;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Descriptioin HttpUtilTest
@@ -74,6 +75,44 @@ public class HttpUtilTest {
         HttpResponse response = HttpUtil.createRequest(Method.POST, url)
                 .body(JSONUtil.toJsonStr(paramMap))
                 .execute();
+        if(response.getStatus() == 200){
+            System.out.println(response.body());
+        }
+    }
+
+    @Test
+    public void testWeater() {
+        String url = "https://aliv18.data.moji.com/whapi/json/alicityweather/forecast24hours";
+        String appcode = "f6f6168e5c1c4ae586ddab17909e9b75";//修改成自己的appcode
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("cityId", "1945");
+
+        HttpResponse response = HttpUtil.createRequest(Method.POST, url)
+                .header("Authorization", "APPCODE " + appcode)
+                .form(param)
+                .execute();
+        if (response.getStatus() == 200) {
+            System.out.println(response.body());
+        }
+    }
+
+    @Test
+    public void testLogisticsInformation(){
+        String host = "https://lhkdcx.market.alicloudapi.com";
+        String path = "/express/query";
+        String url = host+path;
+        String appcode = "3327ec8399984ebb891fc8bcbd603d4b";
+        HashMap<String, Object> bodys = new HashMap<String, Object>();
+        bodys.put("expressNo", "expressNo");
+        bodys.put("waybillNo", "JT5309914590202");
+        bodys.put("mobile", "18835600884");
+
+        HttpResponse response = HttpUtil.createRequest(Method.POST,url)
+                .header("Authorization","APPCODE " + appcode)
+                .header("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+                .form(bodys)
+                .execute();
+
         if(response.getStatus() == 200){
             System.out.println(response.body());
         }
